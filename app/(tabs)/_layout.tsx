835a048afa92afea4c919da1742688c85ef03b93
garfnadeck <serraopinto@gmail.com>
2025-09-18 10:45:17 -0300

    Initial commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+import {Tabs} from "expo-router";
+
+const _Layout = () => {
+  return (
+      <Tabs>
+         <Tabs.Screen name="index" options={{
+             title: "Home",
+             headerShown: false}} />
+      </Tabs>
+  );
+};
+
+export default _Layout;
---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MoviesDetails = () => {
+  return (
+    <View>
+      <Text>MoviesDetails component</Text>
+    </View>
+  );
+};
+
+export default MoviesDetails;
+
+const styles = StyleSheet.create({});
